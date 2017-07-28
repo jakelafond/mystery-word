510c16510dc41878f8e7bf2c01228b3b86133970
Jake LaFond <jakelafond@gmail.com>
2017-07-28 15:04:16 -0400

    some stuff done

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const fs = require('fs');
 const words = fs.readFileSync('/usr/share/dict/words', 'utf-8').toLowerCase().split('\n');
 let underscores = [];
 let guesses = [];
+let counter = 8;
+var secretWord = [];
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,16 +24,32 @@ let normalWord = function() {
 };
 
 let newWord = normalWord();
+console.log(newWord);
 
 for (let x = 0; x < newWord.length; x++) {
   underscores.push('_');
 }
+for (let j = 0; j < newWord.length; j++) {
+  secretWord.push(newWord[j]);
+}
+
+console.log(secretWord);
 
 app.get('/', (req, res) => {
-  res.render('index', { underscores: underscores, guesses: guesses });
+  res.render('index', { underscores: underscores, guesses: guesses, counter: counter });
 });
 
 app.post('/', (req, res) => {
+  if (secretWord.includes(req.body.guess)) {
+    for (let q = 0; q < secretWord.length; q++) {
+      if (secretWord[q] === req.body.guess) {
+        underscores.splice(q, 1, req.body.guess);
+      }
+    }
+  } else {
+    count -= 1;
+    console.log(count);
+  }
   guesses.push(req.body.guess);
   res.redirect('/');
 });
