68dfe60ed2a53de4563cff10e8ac08c1bb01e36a
Jake LaFond <jakelafond@gmail.com>
2017-07-28 13:16:07 -0400

    Making progress

--- app.js
+++ app.js
@@ -5,17 +5,35 @@ const mustacheExpress = require('mustache-express');
 const expressValidator = require('express-validator');
 const fs = require('fs');
 const words = fs.readFileSync('/usr/share/dict/words', 'utf-8').toLowerCase().split('\n');
+let underscores = [];
+let guesses = [];
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 
-app.engine('mst', mustacheExpress());
+app.engine('mustache', mustacheExpress());
 app.set('views', './views');
-app.set('view engine', 'mst');
+app.set('view engine', 'mustache');
+
+let normalWord = function() {
+  let i = Math.floor(Math.random() * words.length);
+  return words[i];
+};
+
+let newWord = normalWord();
+
+for (let x = 0; x < newWord.length; x++) {
+  underscores.push('_');
+}
 
 app.get('/', (req, res) => {
-  res.send('it works');
+  res.render('index', { underscores: underscores, guesses: guesses });
+});
+
+app.post('/', (req, res) => {
+  guesses.push(req.body.guess);
+  res.redirect('/');
 });
 
 app.listen(3000, () => {
